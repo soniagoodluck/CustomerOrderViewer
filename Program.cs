92e1b1c87ab301cb3983ebcdb82e8fd513cd2b20
Sonia <soniagoodluck@yahoo.co.uk>
2021-03-19 11:15:38 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace CustomerOrderViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
